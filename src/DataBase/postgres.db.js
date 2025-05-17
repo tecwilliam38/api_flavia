09db0503863205489887111c6fb44a4e6afce191
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-05-17 17:46:11 -0300

    teste

--- src/DataBase/postgres.db.js
+++ src/DataBase/postgres.db.js
@@ -1,11 +1,6 @@
 import pg from 'pg'
 const { Pool } = pg;
 // Dados novo banco supabase
-// qTBe8bXkmtVpEo2L password
-//  URL https://kpcanhcozznqvfibklhd.supabase.co
-// JWT xFW6e3bktVP5k4Lr+5BmqKyHdWjWyDugRBk4uFnhO8FaK6tffi4TZ9wuujMIEvlNGbTa0DB+IrQ8crJ0EW8vPA==
-// ID kpcanhcozznqvfibklhd
-// API key eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwY2FuaGNvenpucXZmaWJrbGhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MjQ5OTQsImV4cCI6MjA1MTQwMDk5NH0.684EaApGs1HDiRVK3G04SFSvZxbqGaN2orbdWq31D7E
 
 const pool = new Pool({    
     host: 'aws-0-sa-east-1.pooler.supabase.com',
@@ -13,7 +8,6 @@ const pool = new Pool({
     user: 'postgres.kfuglmenqxnbfjgtqnws',    
     database: 'postgres',    
     password: 'Estacio091@@',
-    // pool_mode:"transaction",
 });
 
 // Teste de Conexção
